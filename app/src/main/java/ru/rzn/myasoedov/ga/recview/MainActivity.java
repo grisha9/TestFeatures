d3bc8c3f679b685e7d380fb68098d9ba19fb15b3
Grigoriy Myasoedov <gmyasoedov@newnpr.com>
2016-09-01 14:20:51 +0300

    add retrolambda

--- app/src/main/java/ru/rzn/myasoedov/ga/recview/MainActivity.java
+++ app/src/main/java/ru/rzn/myasoedov/ga/recview/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -37,15 +36,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         rv.setHasFixedSize(true);
         rv.setLayoutManager(new LinearLayoutManager(this));
         getSupportLoaderManager().initLoader(0, null, this);
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!TextUtils.isEmpty(textView.getText())) {
-                    Message message = new Message(textView.getText().toString(), new Date(), true);
-                    textView.setText("");
-                    getContentResolver().insert(MessageContract.CONTENT_URI,
-                            message.toContentValues());
-                }
+        buttonSend.setOnClickListener(v -> {
+            if (!TextUtils.isEmpty(textView.getText())) {
+                Message message = new Message(textView.getText().toString(), new Date(), true);
+                textView.setText("");
+                getContentResolver().insert(MessageContract.CONTENT_URI,
+                        message.toContentValues());
             }
         });
     }
